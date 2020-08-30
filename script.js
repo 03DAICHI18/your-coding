d451c0589549dab3d85a4ef2106d2a232fe3d889
03DAICHI18 <dai.03.chi.18@gmail.com>
2020-08-30 23:32:34 +0900

    faqまで

--- script.js
+++ script.js
@@ -15,4 +15,11 @@ $(function () {
   $('.js-btn').on('click', function () { // js-btnクラスをクリックすると、
     $('.header__nav-in , .btn-line').toggleClass('open'); // メニューとバーガーの線にopenクラスをつけ外しする
   })
+
+  // アコーディオン
+  $(".faq__ac-child:not(:first-of-type)").css("display", "none");
+  $('.faq__ac-parent').on('click', function () {
+    $(this).next().slideToggle();
+    $('.faq__ac-parent').not($(this)).next('.faq__ac-child').slideUp();
+  })
 });
