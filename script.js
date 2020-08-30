385ea6c5daeb57cf239d54289a88c5eb261d03f3
03DAICHI18 <dai.03.chi.18@gmail.com>
2020-08-30 15:30:15 +0900

    ヘッダー、ページ内スムーススクロール

---
+++ script.js
@@ -0,0 +1,18 @@
+$(function () {
+  // ページ内スムーススクロール
+  $('a[href^="#"]').click(function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    var speed = 500;
+    $("html, body").animate({
+      scrollTop: position
+    }, speed, "swing");
+    return false;
+  });
+
+  // ハンバーガーメニュー
+  $('.js-btn').on('click', function () { // js-btnクラスをクリックすると、
+    $('.header__nav-in , .btn-line').toggleClass('open'); // メニューとバーガーの線にopenクラスをつけ外しする
+  })
+});
