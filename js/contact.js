a5827a693d322b5e98630e7c5d84b47a60d923f5
03DAICHI18 <dai.03.chi.18@gmail.com>
2020-09-03 21:45:53 +0900

    コンタクトフォームはGoogleフォームに変更

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,6 @@
 $(function () {
-  const $submitBtn = $('#js-submit')
+  // 送信ボタン活性・非活性
+  const $submitBtn = $('#form_submit_button')
   $('#form input,#form textarea').on('change', function () {
     if (
       $('#form input[type="text"]').val() !== "" &&
@@ -11,5 +12,27 @@ $(function () {
     } else {
       $submitBtn.prop('disabled', true);
     }
+  });
+
+  // 問い合わせ完了時のアクション
+  $('#form').submit(function (event) {
+    var formData = $('#form').serialize();
+    $.ajax({
+      url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdCYUtmn6v4B9uNdTlbiOgkKfvK1Z7CeKSZuOcLuMoLUJldew/formResponse",
+      data: formData,
+      type: "POST",
+      dataType: "xml",
+      statusCode: {
+        0: function () {
+          $(".end-message").slideDown();
+          $(".m-btn").fadeOut();
+          window.location.href = "thanks.html";
+        },
+        200: function () {
+          $(".false-message").slideDown();
+        }
+      }
+    });
+    event.preventDefault();
   });
 });
